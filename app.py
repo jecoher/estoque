6192167bc79146eba68affc3a7b3504d089324f5
jecoher <j3ffh3r9510@gmail.com>
2026-09-25 23:23:06 -0300

    actualizacion centralizacion y utilizacion de funciones

--- app.py
+++ app.py
@@ -1,90 +1,77 @@
-lista_productos = []
+lista_productos = [
+    {"id": 1, "nombre": "ESCOBA", "precio": 15.0, "cantidad": 10},
+    {"id": 2, "nombre": "ESPONJA", "precio": 5.0, "cantidad": 50},
+]
 
-def consecutivo_id(lista):
-    return len(lista) + 1
+def buscar_prodcuto_por_id(lista, id_busca):
+    """
+    none -> si no encuentra producto\n
+    producto -> si lo encuentra
+    """
+    for producto in lista:
+        if producto['id'] == id_busca:
+            return producto
+    return None
 
-def validacion_valores_num_producto(opcion_usuario):
-    '''
-    valida si la opcion digitada por usuario no es negativa ni un espacio en blanco o una str.
-    '''
-    try: 
-        opcion_usuario_float = float(opcion_usuario)
-        if opcion_usuario_float < 0:
-            return None
-        return opcion_usuario_float
-    except ValueError:
-        return(None)
-    
-def validacion_int_user(opcion_usuario):
-    '''
-    valida si la opcion digitada por usuario no es negativa ni un espacio en blanco o una str.
-    '''
-    try: 
-        opcion_usuario_int = int(opcion_usuario)
-        if opcion_usuario_int < 0:
-            return None
-        return opcion_usuario_int
-    except ValueError:
-        return(None)
-
-    
-while True:
-
-    opc_menu_user = input("""
-[1] Registrar producto
-[2] Buscar producto
-[3] Salir
-: """).strip()
-
-    if opc_menu_user == '1':
-        numero_id = consecutivo_id(lista_productos)
-        while True:
-            nombre = input("Digite el nombre del producto: ").strip().upper()
-            if nombre == "":
-                print("Valor vacio, Invalido!")
-                continue
-            break
-
-        while True:
-            precio = input("Digite el precio del producto: R$").strip()
-            precio_verificado = validacion_valores_num_producto(precio)
-            if precio_verificado is None:
-                print("Valor invalido!")
-                continue
-            break
-
-        while True:
-            cantidad = input("Digite la cantidad del producto: ").strip()
-            cantidad_verificado = validacion_valores_num_producto(cantidad)
-            if cantidad_verificado is None:
-                print("Valor invalido!")
-                continue
-            break
-
-        lista_productos.append({"id":numero_id, "nombre":nombre, "precio":precio_verificado, "cantidad": cantidad_verificado})
-        print("Producto adicionado con exito")
-        print(lista_productos)
-
-
-    elif opc_menu_user == '2':
-        usuario_produto = input('Digite el ID del producto a buscar: ').strip()
-        usuario_produto_verificado = validacion_int_user(usuario_produto)
-        if usuario_produto_verificado is None:
-            print("Digite un id valido!")
-            continue
-
-        if len(lista_productos) < 1:
-            print("Lista vacia!")
-            continue
-
-        else:
-            for producto in lista_productos:
-                if producto['id'] == usuario_produto_verificado:
-                    print(producto)
-            else:
-                print("Producto no encontrado!")
-    elif opc_menu_user == '3':
-        print("Saliendo del sistema!")
-        break
+def eliminar_producto_por_id(lista, id_eliminar):
+    """
+    True -> producto eliminado\n
+    False -> producto no encontrado
+    """
+    producto_encontrado = buscar_prodcuto_por_id(lista, id_eliminar)
+    if producto_encontrado is None:
+        return False
     else:
-        print("Opcion Invalida!")
+        lista.remove(producto_encontrado)
+        return True
+        
+def vender_producto(lista, id_vender, cantidad):
+    """
+    None -> producto no encontrado\n
+    False -> cantidad producto menor a solicitada\n
+    Float con valor total a cobrar
+    """ 
+    producto_encontrado = buscar_prodcuto_por_id(lista, id_vender)
+    if producto_encontrado is None:
+        return None
+    elif producto_encontrado['cantidad'] < cantidad:
+        return False
+
+    else:
+        producto_encontrado['cantidad'] -= cantidad
+        total_cobrar = producto_encontrado['precio'] * cantidad
+        return float(total_cobrar)
+
+def reabastecer_con_id(lista, id_reavastecer, cantidad):
+    producto_encontrado = pro
+
+
+id_objetivo = 51
+accion = "eliminar"        
+unidades_operacion = 4
+
+producto_encontrado = buscar_prodcuto_por_id(lista_productos, id_objetivo)
+
+if accion == 'vender':
+    venta = vender_producto(lista_productos, id_objetivo, unidades_operacion)
+    if venta is None:
+        print("Producto no encontrado!")
+    elif venta is False:
+        print("Cantidad para venda menor que estoque!")
+    else:
+        print(f"Venta realizada con exito, total a cobrar {venta}")
+
+
+elif accion == 'reabastecer':
+    producto_encontrado['cantidad'] += unidades_operacion
+    print(f"Fueron adicionados para cod {producto_encontrado['id']} {producto_encontrado['nombre']} {unidades_operacion} unidades. Ahora tiene {producto_encontrado['cantidad']} unidades")
+
+
+elif accion == "eliminar":
+    eliminar = eliminar_producto_por_id(lista_productos, id_objetivo)
+    if eliminar:
+        print(f"Prodcuto eliminado con exito!")
+        print(lista_productos)
+    else:
+        print("prodcuto no encontrado")
+
